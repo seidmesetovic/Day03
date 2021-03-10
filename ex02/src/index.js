c002649d16e9ab474815eda8ab4ac1023a6e399c
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-10 23:51:17 +0100

    Zavrsena ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main () {
+    //Ovo je java komntar//
+    
+    /* document.write("Ovo je isto")
+    document.write("java komentar")
+    */
+    }
+    console.log((main));
+    module.exports = main;
